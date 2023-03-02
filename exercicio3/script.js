975efa4d40a62584e9e2dbed05fbd842763731f3
felipelimars <felipelimars@outlook.com.br>
2023-03-01 23:09:31 -0300

    exercicios resolvidos

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,16 +1,26 @@
-//códigos a serem reescritos
+//Exercicio 3) 
+
+//códigos a serem reescritos utilizando (if ternario)
 
 // a)
-if(nome === "José"){
-	console.log("Oi, Zé!")
-} else {
-	console.log("Olá, " + nome)
-}
+//if(nome === "José"){
+//	console.log("Oi, Zé!")
+//} else {
+//	console.log("Olá, " + nome)
+//}
+const nome = prompt(`Qual seu nome?`)
+nome === `José` ? console.log(`Oi, Zé`) : console.log(`Olá,` + nome)
+
 
 
 // b)
-if(idade >= 18){
-	console.log("Pode tirar carteira de motorista!")
-} else {
-	console.log("Ainda não pode tirar carteira de motorista!")
-}
+//if(idade >= 18){
+//	console.log("Pode tirar carteira de motorista!")
+//} else {
+//	console.log("Ainda não pode tirar carteira de motorista!")
+//}
+const idade = Number(prompt(`Digite sua idade:`))
+idade >= 18 ? console.log(`Pode tirar a carteira de motorista!`) : (`Ainda não pode tirar carteira de motorista!`)
+
+
+// utilizei um prompt para perguntar o nome e a idade do usuário para testar os códigos.
